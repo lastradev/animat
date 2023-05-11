1bfa25a499f4fcd8ba214cb3ee9f261ac988e0bc
Oscar Lastra <olastra@easybroker.com>
2023-05-10 23:06:06 -0600

    Remove unnecessary code in MainActivity

--- app/src/main/java/com/example/animat/MainActivity.kt
+++ app/src/main/java/com/example/animat/MainActivity.kt
@@ -10,24 +10,23 @@ import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import com.example.animat.models.Anime
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
 
 const val USER_NAME_KEY = "name"
 const val USER_AGE_KEY = "age"
+
 class MainActivity : AppCompatActivity() {
     private lateinit var tvWelcome: TextView
     private lateinit var etUserName: EditText
     private lateinit var etUserAge: EditText
     private var name: String = ""
     private var age: Int = 0
-    private var matchedAnimes: ArrayList<Anime> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         Log.d("PREFERENCES", "onCreate")
+
         val bnContinue = findViewById<Button>(R.id.bnContinue)
         tvWelcome = findViewById(R.id.tvWelcome)
         etUserName = findViewById(R.id.etUserName)
@@ -36,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         bnContinue.setOnClickListener {
             name = etUserName.text.toString()
             age = etUserAge.text.toString().toInt()
+
             val sharedPreferences = getSharedPreferences("PREFERENCES", MODE_PRIVATE)
             val editor = sharedPreferences.edit()
 
@@ -47,6 +47,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(i)
         }
     }
+
     override fun onResume() {
         Log.d("PREFERENCES", "onResume")
 
@@ -54,13 +55,6 @@ class MainActivity : AppCompatActivity() {
             val sharedPreferences = getSharedPreferences("PREFERENCES", MODE_PRIVATE)
             name = sharedPreferences.getString(USER_NAME_KEY, "").toString()
             age = sharedPreferences.getInt(USER_AGE_KEY, 0)
-            val gson = Gson()
-            val matchedAnimesJson = sharedPreferences.getString(MATCHED_ANIMES_KEY, null)
-            if (matchedAnimesJson != null) {
-                val type = object : TypeToken<ArrayList<Anime>>() {}.type
-                matchedAnimes = gson.fromJson(matchedAnimesJson, type)
-            }
-            Log.d("LOGIN", matchedAnimes.toString())
         }
 
         if (name != ""){
@@ -71,22 +65,27 @@ class MainActivity : AppCompatActivity() {
 
         super.onResume()
     }
+
     override fun onStart() {
         Log.d("PREFERENCES", "onStart")
         super.onStart()
     }
+
     override fun onPause() {
         Log.d("PREFERENCES", "onPause")
         super.onPause()
     }
+
     override fun onDestroy() {
         Log.d("PREFERENCES", "onDestroy")
         super.onDestroy()
     }
+
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_animes, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == R.id.option_menu_list) {
             val i = Intent(this,MatchedAnimesActivity::class.java)
