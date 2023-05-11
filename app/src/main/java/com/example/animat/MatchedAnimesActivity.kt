770e50acad7aa3bb584b1e9e1b5bfef9aae44a15
Andrea Sosa <andrea.sosa01@iest.edu.mx>
2023-05-11 12:42:57 -0600

    Created landscape view for matchedAnimes, added fab button to recycler

--- app/src/main/java/com/example/animat/MatchedAnimesActivity.kt
+++ app/src/main/java/com/example/animat/MatchedAnimesActivity.kt
@@ -1,7 +1,9 @@
 package com.example.animat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Configuration
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.animat.adapters.MatchedAnimeAdapter
@@ -16,6 +18,7 @@ class MatchedAnimesActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_matched_animes)
 
+        val orientation = resources.configuration.orientation
         val sharedPreferences = getSharedPreferences("PREFERENCES", MODE_PRIVATE)
         val gson = Gson()
         val matchedAnimesJson = sharedPreferences.getString(MATCHED_ANIMES_KEY, null)
@@ -24,10 +27,17 @@ class MatchedAnimesActivity: AppCompatActivity() {
             matchedAnimes = gson.fromJson(matchedAnimesJson, type)
         }
 
-        val linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         val recycler = findViewById<RecyclerView>(R.id.recyclerAnime)
 
-        recycler.layoutManager = linearLayoutManager
+
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            val gridLayoutManager = GridLayoutManager(this, 2)
+            recycler.layoutManager = gridLayoutManager
+        } else {
+            val linearLayoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+            recycler.layoutManager = linearLayoutManager
+        }
         recycler.adapter = MatchedAnimeAdapter(matchedAnimes)
     }
 }
